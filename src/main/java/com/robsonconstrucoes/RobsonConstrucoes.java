924d34781f96d3e253fe2aa83fcea4d69573edc0
Luizhenriquerios <henriquerios4@gmail.com>
2023-08-15 16:05:27 -0300

    feat: add menu

--- src/main/java/com/robsonconstrucoes/RobsonConstrucoes.java
+++ src/main/java/com/robsonconstrucoes/RobsonConstrucoes.java
@@ -1,4 +1,52 @@
 package com.robsonconstrucoes;
 
+import java.util.Scanner;
 public class RobsonConstrucoes {
+
+public static void main(String[] args) {
+    RobsonConstrucoes empresa = new RobsonConstrucoes();
+    Scanner scanner = new Scanner(System.in);
+
+    while (true){
+        System.out.println("\nEscolha uma opção:");
+        System.out.println("1. Cadastrar cargo");
+        System.out.println("2. Cadastrar funcionário");
+        System.out.println("3. Mostrar relatório");
+        System.out.println("4. Mostrar total de salário por cargo");
+        System.out.println("5. Sair");
+
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.print("Digite o salário do novo cargo: ");
+                double salario = scanner.nextDouble();
+                empresa.cadastrarCargo(salario);
+                break;
+            case 2:
+                System.out.print("Digite o código do funcionário: ");
+                int codigo = scanner.nextInt();
+                System.out.print("Digite o nome do funcionário: ");
+                String nome = scanner.next();
+                System.out.print("Digite o código do cargo: ");
+                int codigoCargo = scanner.nextInt();
+                empresa.cadastrarFuncionario(codigo, nome, codigoCargo);
+                break;
+            case 3:
+                empresa.mostrarRelatorio();
+                break;
+            case 4:
+                System.out.print("Digite o código do cargo: ");
+                int codigoCargoTotal = scanner.nextInt();
+                empresa.mostrarTotalSalarioPorCargo(codigoCargoTotal);
+                break;
+            case 5:
+                System.out.println("Saindo...");
+                scanner.close();
+                System.exit(0);
+            default:
+                System.out.println("Opção inválida. Escolha uma opção válida.");
+        }
+    }
+}
 }
